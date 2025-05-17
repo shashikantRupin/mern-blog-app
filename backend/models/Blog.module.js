da00011390acc234702b2f6eba0bdb2e917c13da
Shashikant Rupin <shashikantrupin123@gmail.com>
2025-05-17 09:56:43 +0530

    backend blog created date added to server

--- backend/models/Blog.module.js
+++ backend/models/Blog.module.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
-const BlogSchema = mongoose.Schema({
+const BlogSchema = mongoose.Schema(
+  {
     title: { type: String, required: true },
     content: { type: String, required: true },
     auth_email: { type: String, required: true },
     user_id: { type: String, required: true },
-    type: { type: String, enum: ['tech', 'news', 'food', 'health', 'other'] },
-    imageUrl: { type: String }
-});
+    type: { type: String, enum: ["tech", "news", "food", "health", "other"] },
+    imageUrl: { type: String },
+  },
+  { timestamps: true }
+);
+
+
 
 const BlogModel = mongoose.model('Blog', BlogSchema);
 module.exports = BlogModel;
