dfc730bb92ba9eba501b3b05ef30abcb3b9213a8
Shashikant Rupin <shashikantrupin123@gmail.com>
2025-05-17 16:03:24 +0530

    improve hamburger menu responsiveness

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -25,39 +25,42 @@ const Navbar = () => {
 
   return (
     <nav>
-      <Link to="/" className="title">
-        <img
-          src="https://static.vecteezy.com/system/resources/thumbnails/024/553/534/small/lion-head-logo-mascot-wildlife-animal-illustration-generative-ai-png.png"
-          alt="logo"
-          style={{ height: "80px" }}
-        />
-      </Link>
+      <div className="title-link-wrapper">
+        <Link to="/" className="title">
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/024/553/534/small/lion-head-logo-mascot-wildlife-animal-illustration-generative-ai-png.png"
+            alt="logo"
+            style={{ height: "80px" }}
+            className="logo-blog"
+          />
+        </Link>
 
-      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
-        <span></span>
-        <span></span>
-        <span></span>
+        <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <ul className={menuOpen ? "open" : "nav-list"}>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/blogs">Blogs</NavLink>
+          </li>
+          <li>
+            <NavLink to="/myBlogs">My-Blogs</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/create">Create</NavLink>
+          </li>
+        </ul>
       </div>
 
-      <ul className={menuOpen ? "open" : ""}>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/blogs">Blogs</NavLink>
-        </li>
-        <li>
-          <NavLink to="/myBlogs">My-Blogs</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/create">Create</NavLink>
-        </li>
-      </ul>
-
-      <div style={{ marginRight: "30px" }}>
+      <div style={{ marginRight: "30px" }} className="login-logout">
         {loggedIn ? (
           <>
             <span style={{ marginRight: "15px", fontWeight: "bold" }}>
@@ -65,6 +68,7 @@ const Navbar = () => {
             </span>
             <button
               onClick={handleLogout}
+              className="log-out-btn"
               style={{
                 padding: "8px 12px",
                 borderRadius: "5px",
